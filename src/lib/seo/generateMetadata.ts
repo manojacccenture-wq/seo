42c37e3a01d56cdcceeddd63c54780a67ef46ebf
manojacccenture-wq <manojacccenture@gmail.com>
2026-03-12 17:42:38 +0530

    Fix:Typescript fix

--- src/lib/seo/generateMetadata.ts
+++ src/lib/seo/generateMetadata.ts
@@ -1,17 +1,27 @@
 import { siteConfig } from "./seo.config"
 
+type SEOProps = {
+  title: string
+  description?: string
+  path?: string
+  image?: string
+  keywords?: string[]
+}
+
 export function generateSEO({
   title,
   description,
   path = "",
-  image = "/og-image.png"
-}) {
+  image = "/og-image.png",
+  keywords
+}: SEOProps) {
+
   const url = `${siteConfig.siteUrl}${path}`
 
   return {
     title: `${title} | ${siteConfig.siteName}`,
     description: description || siteConfig.description,
-    keywords: siteConfig.keywords,
+    keywords: keywords || siteConfig.keywords,
 
     openGraph: {
       title,
